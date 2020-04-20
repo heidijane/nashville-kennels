d639b7e4f5cc73fa44ce0f6c250c91db97c94d02
heidijane <heidi@heidisprouse.com>
2020-04-20 14:19:59 -0500

    animals, employees, customers, and locations now appear on the DOM

---
+++ src/components/customers/Customer.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <section className="customer">
+        <h3 className="customer__name">Heidi Sprouse</h3>
+        <div className="customer__location">200 Someplace Street</div>
+    </section>
+)
