0cf9fc5b4034e215b22f08a00d26bdffd82bb390
heidijane <heidi@heidisprouse.com>
2020-04-24 11:01:30 -0500

    animal details now show as a modal when the name is clicked from the search results, also I must be an outlier that capitalizes names when I type them in search so I made the animal search bar case insensitive

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,7 +29,6 @@ export default () => {
                                 <h2>Nashville Kennels</h2>
                                 <small>Loving care when you're not there.</small>
                                 <LocationList />
-                                <AnimalList />
                                 <CustomerList />
                                 <EmployeeList />
                             </div>
--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -1,13 +1,26 @@
 import React, { useState, useContext, useEffect } from "react"
 import { AnimalContext } from "../animal/AnimalProvider"
+import { Modal, ModalHeader, ModalBody } from "reactstrap"
+import Animal from "../animal/Animal"
+import { CustomerContext } from "../customers/CustomerProvider"
+import { LocationContext } from "../locations/LocationProvider"
+
+
 
 export const SearchResults = ({ searchTerms }) => {
     const { animals } = useContext(AnimalContext)
-    const [ filteredAnimals, setFiltered ] = useState([])
+    const { customers } = useContext(CustomerContext)
+    const { locations } = useContext(LocationContext)
+
+    const [filteredAnimals, setFiltered] = useState([])
+    const [selectedAnimal, setAnimal] = useState({animal: {id:0}, location: null, customer: null})
+
+    const [modal, setModal] = useState(false)
+    const toggle = () => setModal(!modal)
 
     useEffect(() => {
-        if (searchTerms !== "") {
-            const subset = animals.filter(animal => animal.name.toLowerCase().includes(searchTerms))
+        if (searchTerms !== null && searchTerms !== "") {
+            const subset = animals.filter(animal => animal.name.toLowerCase().includes(searchTerms.toLowerCase()))
             setFiltered(subset)
         } else {
             setFiltered([])
@@ -19,9 +32,28 @@ export const SearchResults = ({ searchTerms }) => {
             <h3>Results</h3>
             <div className="animals">
                 {
-                    filteredAnimals.map(animal => <div key={ animal.id }>{ animal.name }</div>)
+                    filteredAnimals.map(animal => <div
+                        className="fakeLink href"
+                        key={ animal.id }
+                        onClick={() => {
+                            const location = locations.find(l => l.id === animal.locationId)
+                            const customer = customers.find(c => c.id === animal.customerId)
+
+                            setAnimal({ animal, location, customer })
+                            toggle()
+                        }}
+                    >{animal.name}</div>)
                 }
             </div>
+
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>
+                    { selectedAnimal.animal.name }
+                </ModalHeader>
+                <ModalBody>
+                    <Animal key={selectedAnimal.animal.id} {...selectedAnimal} />
+                </ModalBody>
+            </Modal>
         </div>
     )
 }
